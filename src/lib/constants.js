74e1282c86c17f2cba35cf26830090ecd47f0fc6
Robert Reed <robert.mc.reed@gmail.com>
2019-11-28 00:46:20 -0700

    CamTwist constants

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -4,3 +4,9 @@ const c = module.exports = {};
 
 c.prod = process.env.NODE_ENV === 'production';
 c.publicDir = path.resolve(`${__dirname}/../public`);
+c.volumeName = 'CamTwist_3.4.3';
+c.dmgName = `${c.volumeName}.dmg`;
+c.camTwistDmgPath = `~/Downloads/${c.dmgName}`;
+c.camTwistAppPath = '/Applications/CamTwist/CamTwist.app';
+c.pkgPath = `/Volumes/${c.volumeName}/CamTwist.pkg`;
+c.camTwistUrl = `http://camtwiststudio.com/beta/${c.dmgName}`;
