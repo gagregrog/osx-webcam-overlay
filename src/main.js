b5eb7720fb799a09552d56073f7397b414e04853
Robert Reed <robert.mc.reed@gmail.com>
2019-11-28 00:48:57 -0700

    initial camtwist flow

--- src/main.js
+++ src/main.js
@@ -1,14 +1,23 @@
+const open = require('open');
+
 const u = require('./lib/util');
 const server = require('./lib/server');
 const handlePort = require('./lib/handlePort');
+const handleCamTwist = require('./lib/handleCamTwist');
 
 const main = async () => {
+  await handleCamTwist();
   const port = await handlePort(3000);
+
   if (!port) {
     return u.warn('Project could not be started.');
   }
 
-  return server.start(port);
+  await server.start(port);
+  const url = `http://localhost:${port}`;
+
+  u.log(`Opening browser to ${url}`);
+  open(url);
 };
 
 module.exports = main;
